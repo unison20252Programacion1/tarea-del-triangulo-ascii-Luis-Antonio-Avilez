8cca3f8fa335c97222e3b033e1c7c50f6dc53770
Luis-Antonio-Avilez <avilezortegam2@gmail.com>
2025-11-21 14:26:25 -0700

    Fix indentation for triangulo_simetrico call

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
         
 
     # TODO: llamar a la función triangulo_simetrico con los parámetros m y s
-triangulo_simetrico(m, s)
+    triangulo_simetrico(m, s)
 
 if __name__ == "__main__":
     main()
